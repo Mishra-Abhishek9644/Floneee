23d581a365538cb62112bd675572035958c2e542
Avanish Yadav <158454874+Avanish-03@users.noreply.github.com>
2025-12-11 14:48:36 +0530

    feat: add Blog component and integrate it into the Home page

---
+++ components/Blog.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Blog = () => {
+    return (
+        <>
+            <div className='md:py-10'>
+                <div className='flex justify-center items-center'>
+                    <h2 className='flex items-center'> <hr className='w-20 border' /> <span className='px-5 text-2xl font-semibold'>OUR BLOG</span>  <hr className='w-20 border' /></h2>
+                </div>
+                <div className='flex justify-evenly items-center'>
+                    <div>
+                        <div>
+                            <img src="" alt="" />
+                        </div>
+                        <div>
+                            <h2>blog</h2>
+                            <p>By Admin</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <img src="" alt="" />
+                        </div>
+                        <div>
+                            <h2>blog</h2>
+                            <p>By Admin</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <img src="" alt="" />
+                        </div>
+                        <div>
+                            <h2>blog</h2>
+                            <p>By Admin</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Blog
