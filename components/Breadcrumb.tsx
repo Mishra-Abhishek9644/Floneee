89b5d63e2803d597c765cfaa095ff79b89077265
Avanish Yadav <158454874+Avanish-03@users.noreply.github.com>
2025-12-12 14:44:43 +0530

    feat: add ProductDetails component and update Navbar and Footer styles

--- components/Breadcrumb.tsx
+++ components/Breadcrumb.tsx
@@ -9,8 +9,8 @@ const Breadcrumb = () => {
     const page = pathname.replace("/", "");
     return (
         <>
-            <div className='flex justify-center items-center py-10 bg-[#f7f7f7] mt-20'>
-                <div className=''>
+            <div className='flex justify-center items-center py-10 bg-[#f7f7f7] mt-16'>
+                <div className='uppercase'>
                     <Link href="/" className='text-gray-500 hover:text-black'>Home</Link>
                     <span className='px-2'> / </span>
                     {page}
