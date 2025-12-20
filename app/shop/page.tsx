4a31a0c3ed909d55c96a6d98836513539808690d
Abhishek Mishra <mishraabhishek9644@gmail.com>
2025-12-20 12:02:20 +0530

    bugs

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import ShopClient from "./ShopClient";
 
 export default function Page() {
-  return <ShopClient />;
+  return (
+    <Suspense fallback={<div>Loading products...</div>}>
+      <ShopClient />
+    </Suspense>
+  );
 }
