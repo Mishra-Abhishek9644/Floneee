34b4a1ec7f0f0f501dda951f31675b6cb1cca529
Avanish Yadav <158454874+Avanish-03@users.noreply.github.com>
2025-12-25 15:33:56 +0530

    feat: enhance AdminDashboard with category and product management features, including image upload and dynamic category selection

--- app/account/admin/page.tsx
+++ app/account/admin/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useSelector, useDispatch } from "react-redux";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Breadcrumb from "@/components/Breadcrumb";
 import { RootState } from "@/Store";
@@ -13,20 +13,46 @@ const AdminDashboard = () => {
   const dispatch = useDispatch();
   const user = useSelector((state: RootState) => state.login.currentUser);
 
-  const [category, setCategory] = useState({ name: "", slug: "" });
-  const [product, setProduct] = useState({
+  const [addOption, setAddOption] = useState("product");
+  const [storeCat, setStoreCat] = useState<any[]>([]);
+
+  const [category, setCategory] = useState({ name: "" });
+  const [product, setProduct] = useState<{
+    title: string;
+    price: string;
+    image: File | null;
+    description: string;
+    categoryId: string;
+    stock: string;
+    sizes: string;
+    colors: string;
+  }>({
     title: "",
     price: "",
-    image: "",
+    image: null,
     description: "",
     categoryId: "",
     stock: "",
     sizes: "",
     colors: "",
   });
+  
+  useEffect(() => {
+    const loadCat = async () => {
+      const res = await fetch("/api/admin/category", {
+        credentials: "include",
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setStoreCat(data.storeCat || data);
+      }
+    };
+    loadCat();
+  }, [])
+  
 
+  
   if (!user) return null;
-
   /* ---------------- LOGOUT ---------------- */
   const handleLogout = async () => {
     try {
@@ -45,16 +71,17 @@ const AdminDashboard = () => {
 
   /* ---------------- ADD CATEGORY ---------------- */
   const addCategory = async () => {
+
     try {
-      await fetch("/api/admin/category", {
+      const res = await fetch("/api/admin/category", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include",
         body: JSON.stringify(category),
       });
-
+      console.log(res);
       toast.success("Category added");
-      setCategory({ name: "", slug: "" });
+      setCategory({ name: "" });
     } catch {
       toast.error("Failed to add category");
     }
@@ -62,31 +89,41 @@ const AdminDashboard = () => {
 
   /* ---------------- ADD PRODUCT ---------------- */
   const addProduct = async () => {
-    try {
-      await fetch("/api/admin/product", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        credentials: "include",
-        body: JSON.stringify({
-          ...product,
-          price: Number(product.price),
-          stock: Number(product.stock),
-          sizes: product.sizes.split(","),
-          colors: product.colors.split(","),
-        }),
-      });
-
-      toast.success("Product added");
-    } catch {
-      toast.error("Failed to add product");
+    if (!product.image) {
+      toast.error("Please select an image");
+      return;
     }
+
+    const formData = new FormData();
+    formData.append("title", product.title);
+    formData.append("price", product.price);
+    formData.append("stock", product.stock);
+    formData.append("description", product.description);
+    formData.append("categoryId", product.categoryId);
+    formData.append("sizes", product.sizes);
+    formData.append("colors", product.colors);
+    formData.append("image", product.image);
+
+    const res = await fetch("/api/admin/product", {
+      method: "POST",
+      credentials: "include",
+      body: formData,
+    });
+
+    if (!res.ok) {
+      toast.error("Failed to add product");
+      return;
+    }
+
+    toast.success("Product added");
   };
+
 
   return (
     <>
       <Breadcrumb />
 
-      <div className="max-w-6xl mx-auto py-20">
+      <div className="max-w-6xl mx-auto md:px-20 px-5 py-20">
         <div className="flex justify-between items-center mb-6">
           <h1 className="text-2xl font-bold">Admin Panel</h1>
 
@@ -106,47 +143,77 @@ const AdminDashboard = () => {
           <p className="text-purple-600 font-semibold">Role: {user.role}</p>
         </div>
 
-        {/* ADD CATEGORY */}
-        <div className="bg-white p-6 shadow rounded mb-10">
-          <h2 className="font-semibold mb-4">Add Category</h2>
-          <input
-            placeholder="Category Name"
-            className="border p-2 w-full mb-2"
-            value={category.name}
-            onChange={(e) => setCategory({ ...category, name: e.target.value })}
-          />
-          <input
-            placeholder="Slug"
-            className="border p-2 w-full mb-2"
-            value={category.slug}
-            onChange={(e) => setCategory({ ...category, slug: e.target.value })}
-          />
-          <button
-            onClick={addCategory}
-            className="bg-purple-600 text-white px-4 py-2"
-          >
-            Add Category
-          </button>
+        <div className="my-5">
+          <div className="flex gap-4 border-b border-gray-500 w-fit">
+            <button className={`${addOption === "product" ? "border-b font-bold text-purple-600" : "border-none"}  pb-2 cursor-pointer`} onClick={() => setAddOption("product")}>Add Products</button>
+            <button className={`${addOption === "category" ? "border-b font-bold text-purple-600" : "border-none"} pb-2 cursor-pointer`} onClick={() => setAddOption("category")}>Add Categories</button>
+          </div>
         </div>
 
-        {/* ADD PRODUCT */}
-        <div className="bg-white p-6 shadow rounded">
-          <h2 className="font-semibold mb-4">Add Product</h2>
-          <input className="border p-2 w-full mb-2" placeholder="Title" onChange={(e) => setProduct({ ...product, title: e.target.value })} />
-          <input className="border p-2 w-full mb-2" placeholder="Price" onChange={(e) => setProduct({ ...product, price: e.target.value })} />
-          <input className="border p-2 w-full mb-2" placeholder="Image URL" onChange={(e) => setProduct({ ...product, image: e.target.value })} />
-          <input className="border p-2 w-full mb-2" placeholder="Category ID" onChange={(e) => setProduct({ ...product, categoryId: e.target.value })} />
-          <input className="border p-2 w-full mb-2" placeholder="Stock" onChange={(e) => setProduct({ ...product, stock: e.target.value })} />
-          <input className="border p-2 w-full mb-2" placeholder="Sizes (S,M,L)" onChange={(e) => setProduct({ ...product, sizes: e.target.value })} />
-          <input className="border p-2 w-full mb-2" placeholder="Colors (Red,Blue)" onChange={(e) => setProduct({ ...product, colors: e.target.value })} />
-          <textarea className="border p-2 w-full mb-2" placeholder="Description" onChange={(e) => setProduct({ ...product, description: e.target.value })} />
-          <button
-            onClick={addProduct}
-            className="bg-purple-600 text-white px-4 py-2"
-          >
-            Add Product
-          </button>
-        </div>
+        {addOption === "category" && (
+          <>
+            <div className="bg-white p-6 shadow rounded mb-10">
+              <h2 className="font-semibold mb-4">Add Category</h2>
+              <input
+                placeholder="Category Name"
+                className="border border-gray-300 outline-hidden p-2 w-full mb-2"
+                value={category.name}
+                onChange={(e) => setCategory({ ...category, name: e.target.value })}
+                required
+              />
+
+              <button
+                onClick={addCategory}
+                className="bg-purple-600 text-white px-4 py-2"
+              >
+                Add Category
+              </button>
+            </div>
+          </>
+        )}
+
+        {addOption === "product" && (
+          <>
+            <div className="bg-white p-6 shadow rounded">
+              <h2 className="font-semibold mb-4">Add Product</h2>
+              <div className="grid grid-cols-2 gap-8">
+                <input className="border border-gray-400 outline-hidden p-2 w-full mb-2" placeholder="Title" onChange={(e) => setProduct({ ...product, title: e.target.value })} />
+                <input className="border border-gray-400 outline-hidden p-2 w-full mb-2" placeholder="Price" onChange={(e) => setProduct({ ...product, price: e.target.value })} />
+              </div>
+              <div className="grid grid-cols-2 gap-8">
+                <input type="file" accept="image/*" className="border border-gray-400 outline-hidden p-2 w-full mb-2" onChange={(e) => setProduct({ ...product, image: e.target.files?.[0] || null })} />
+                <select
+                  className="border p-2 w-full mb-2"
+                  value={product.categoryId}
+                  onChange={(e) =>
+                    setProduct({ ...product, categoryId: e.target.value })
+                  }
+                >
+                  <option value="">Select Category</option>
+                  {storeCat.map((cat) => (
+                    <option key={cat._id} value={cat._id}>
+                      {cat.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="grid grid-cols-2 gap-8">
+                <input className="border border-gray-400 outline-hidden p-2 w-full mb-2" placeholder="Stock" onChange={(e) => setProduct({ ...product, stock: e.target.value })} />
+                <input className="border border-gray-400 outline-hidden p-2 w-full mb-2" placeholder="Sizes (S,M,L)" onChange={(e) => setProduct({ ...product, sizes: e.target.value })} />
+              </div>
+              <div className="grid grid-cols-2 gap-8">
+                <input className="border border-gray-400 outline-hidden p-2 w-full mb-2" placeholder="Colors (Red,Blue)" onChange={(e) => setProduct({ ...product, colors: e.target.value })} />
+                <textarea className="border border-gray-400 outline-hidden p-2 w-full mb-2" placeholder="Description" onChange={(e) => setProduct({ ...product, description: e.target.value })} />
+              </div>
+              <button
+                onClick={addProduct}
+                className="bg-purple-600 text-white px-4 py-2"
+              >
+                Add Product
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
